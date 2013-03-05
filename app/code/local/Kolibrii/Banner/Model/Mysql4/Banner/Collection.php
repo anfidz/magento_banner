e84330f77c2a853f8fcf99ad7e1aad75bc1c1c15
muhammad hafidz istihori <anfidz@gmail.com>
2013-03-05 14:49:17 +0700

    Edit file Model 3 Collection.php
    Edit file Model 3 Collection.php

---
+++ app/code/local/Kolibrii/Banner/Model/Mysql4/Banner/Collection.php
@@ -0,0 +1,1 @@
+    <?php         class Kolibrii_Banner_Model_Mysql4_Banner_Collection extends Mage_Core_Model_Mysql4_Collection_Abstract    {        public function _construct()        {            //parent::__construct();            $this->_init('banner/banner');        }    }
