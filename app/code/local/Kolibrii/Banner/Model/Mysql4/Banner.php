417d95f470fe400a5a878daaf15ec0bec26502ff
muhammad hafidz istihori <anfidz@gmail.com>
2013-03-05 14:46:33 +0700

    edit models 2 Banner.php
    edit models 2 Banner.php

---
+++ app/code/local/Kolibrii/Banner/Model/Mysql4/Banner.php
@@ -0,0 +1,1 @@
+    <?php         class Kolibrii_Banner_Model_Mysql4_Banner extends Mage_Core_Model_Mysql4_Abstract    {        public function _construct()        {               $this->_init('banner/banner', 'banner_id');        }    }
