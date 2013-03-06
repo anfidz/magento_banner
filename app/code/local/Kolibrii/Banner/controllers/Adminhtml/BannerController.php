c0dac1a65804e77b6fd8d88ddb3cf4f3c333607e
muhammad hafidz istihori <anfidz@gmail.com>
2013-03-06 15:41:18 +0700

    add BannerController.php
    add BannerController.php

---
+++ app/code/local/Kolibrii/Banner/controllers/Adminhtml/BannerController.php
@@ -0,0 +1,109 @@
+<?php
+ 
+class Kolibrii_Banner_Adminhtml_BannerController extends Mage_Adminhtml_Controller_Action
+{
+ 
+    protected function _initAction()
+    {
+        $this->loadLayout()
+            ->_setActiveMenu('banner/items')
+            ->_addBreadcrumb(Mage::helper('adminhtml')->__('Items Manager'), Mage::helper('adminhtml')->__('Item Manager'));
+        return $this;
+    }   
+   
+    public function indexAction() {
+        $this->_initAction();       
+        $this->_addContent($this->getLayout()->createBlock('banner/adminhtml_banner'));
+        $this->renderLayout();
+    }
+ 
+    public function editAction()
+    {
+        $bannerId     = $this->getRequest()->getParam('id');
+        $bannerModel  = Mage::getModel('banner/banner')->load($bannerId);
+ 
+        if ($bannerModel->getId() || $bannerId == 0) {
+ 
+            Mage::register('banner_data', $bannerModel);
+ 
+            $this->loadLayout();
+            $this->_setActiveMenu('banner/items');
+           
+            $this->_addBreadcrumb(Mage::helper('adminhtml')->__('Item Manager'), Mage::helper('adminhtml')->__('Item Manager'));
+            $this->_addBreadcrumb(Mage::helper('adminhtml')->__('Item News'), Mage::helper('adminhtml')->__('Item News'));
+           
+            $this->getLayout()->getBlock('head')->setCanLoadExtJs(true);
+           
+            $this->_addContent($this->getLayout()->createBlock('banner/adminhtml_banner_edit'))
+                 ->_addLeft($this->getLayout()->createBlock('banner/adminhtml_banner_edit_tabs'));
+               
+            $this->renderLayout();
+        } else {
+            Mage::getSingleton('adminhtml/session')->addError(Mage::helper('banner')->__('Item does not exist'));
+            $this->_redirect('*/*/');
+        }
+    }
+   
+    public function newAction()
+    {
+        $this->_forward('edit');
+    }
+   
+    public function saveAction()
+    {
+        if ( $this->getRequest()->getPost() ) {
+            try {
+                $postData = $this->getRequest()->getPost();
+                $bannerModel = Mage::getModel('banner/banner');
+               
+                $bannerModel->setId($this->getRequest()->getParam('id'))
+                    ->setTitle($postData['title'])
+                    ->setLink($postData['link'])
+                    ->setImages($postData['images'])
+                    ->save();
+               
+                Mage::getSingleton('adminhtml/session')->addSuccess(Mage::helper('adminhtml')->__('Item was successfully saved'));
+                Mage::getSingleton('adminhtml/session')->set<Module>Data(false);
+ 
+                $this->_redirect('*/*/');
+                return;
+            } catch (Exception $e) {
+                Mage::getSingleton('adminhtml/session')->addError($e->getMessage());
+                Mage::getSingleton('adminhtml/session')->set<Module>Data($this->getRequest()->getPost());
+                $this->_redirect('*/*/edit', array('id' => $this->getRequest()->getParam('id')));
+                return;
+            }
+        }
+        $this->_redirect('*/*/');
+    }
+   
+    public function deleteAction()
+    {
+        if( $this->getRequest()->getParam('id') > 0 ) {
+            try {
+                $<module>Model = Mage::getModel('<module>/<module>');
+               
+                $<module>Model->setId($this->getRequest()->getParam('id'))
+                    ->delete();
+                   
+                Mage::getSingleton('adminhtml/session')->addSuccess(Mage::helper('adminhtml')->__('Item was successfully deleted'));
+                $this->_redirect('*/*/');
+            } catch (Exception $e) {
+                Mage::getSingleton('adminhtml/session')->addError($e->getMessage());
+                $this->_redirect('*/*/edit', array('id' => $this->getRequest()->getParam('id')));
+            }
+        }
+        $this->_redirect('*/*/');
+    }
+    /**
+     * Product grid for AJAX request.
+     * Sort and filter result for example.
+     */
+    public function gridAction()
+    {
+        $this->loadLayout();
+        $this->getResponse()->setBody(
+               $this->getLayout()->createBlock('<module>/adminhtml_<module>_grid')->toHtml()
+        );
+    }
+}
